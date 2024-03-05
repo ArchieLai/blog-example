5fc069c087d23590f19695fe30ac9bc96ae74aae
Archie <archie.weichi@gmail.com>
2024-03-05 10:53:07 +0800

    modify input validation

--- blog/src/component/edit.tsx
+++ blog/src/component/edit.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useState, useContext } from 'react';
+import { useState, useContext, useEffect } from 'react';
 import Backdrop from '@mui/material/Backdrop';
 import { EditProps } from '@/types/component';
 import updateIssue from '@/service/updateIssue';
@@ -13,6 +13,8 @@ const Edit = (props: EditProps) => {
     title: props?.title,
     body: props?.body,
   });
+  const [valBody, setValBody] = useState(false);
+  const [valTitle, setValTitle] = useState(false);
   const [open, setOpen] = useState(false);
 
   const handleClose = () => {
@@ -23,15 +25,33 @@ const Edit = (props: EditProps) => {
   };
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const formData = new FormData(e.currentTarget);
-    const res = await updateIssue(code, formData, props.id);
-
-    //update context
-    if (res.number !== undefined) {
-      const prevData = data.filter((record) => { return record.id !== Number(res.number)});
-      setData([...prevData, {id: res.number, title: res.title, body: res.body, created_at: res.created_at}]);
+    if (!valBody && !valTitle) {
+      const formData = new FormData(e.currentTarget);
+      const res = await updateIssue(code, formData, props.id);
+      //update context
+      if (res.number !== undefined) {
+        const prevData = data.filter((record) => { return record.id !== Number(res.number)});
+        setData([...prevData, {id: res.number, title: res.title, body: res.body, created_at: res.created_at}]);
+      }
     }
   }
+
+  // validation
+  useEffect(() => {
+    const validation = () => {
+      if (formData.body.length < 30) {
+        setValBody(true);
+      } else {
+        setValBody(false);
+      }
+      if (formData.title.length === 0) {
+        setValTitle(true);
+      } else { 
+        setValTitle(false);
+      }
+    }
+    validation();
+  }, [formData.body, formData.title]);
 
   return (
     <div>
@@ -39,7 +59,7 @@ const Edit = (props: EditProps) => {
       <Backdrop open={open}>
         <div className='bg-white w-[90vw] lg:w-[1000px] p-5 rounded-lg'>
           <div className='text-right'><button onClick={handleClose}><CloseIcon /></button></div>
-          <form onSubmit={handleSubmit} method="post" className='bg-white flex flex-col gap-3'>
+          <form onSubmit={handleSubmit} method="post" className='bg-white flex flex-col gap-2'>
             <label className='text-md lg:text-xl'>編輯標題</label>
             <input 
               type='text' 
@@ -49,13 +69,15 @@ const Edit = (props: EditProps) => {
               onChange={(e) => setFormData({ ...formData, title: e.target.value })}
               className='border-2 border-neutral-400 rounded-sm p-2'
             />
+            {valTitle && <p className='text-red-500 text-center'>標題不得為空</p>}
             <label className='text-md lg:text-xl'>編輯內容</label>
             <textarea 
               name='body' 
               value={formData.body}
               onChange={(e) => setFormData({ ...formData, body: e.target.value })}
-              className='h-[250px] border-2 border-neutral-400 rounded-sm p-2' 
+              className='h-[200px] border-2 border-neutral-400 rounded-sm p-2' 
             />
+            {valBody && <p className='text-red-500 text-center'>內容不得少於30字</p>}
             <div className='text-center'>
               <button type="submit" onClick={handleClose} className='w-[60px] lg:w-[100px] h-[30px] lg:h-[50px] bg-neutral-600 text-white rounded-md text-sm lg:text-lg'>更新</button>
             </div>              
