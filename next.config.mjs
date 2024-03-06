4421b9e7d74c5e69efa1735fc87b758aba52fbcf
Archie <archie.weichi@gmail.com>
2024-03-06 23:43:27 +0800

    rm yml

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: "/blog-example",
-  output: "standalone",
-  reactStrictMode: true,
   logging: {
     fetches: {
       fullUrl: true,
